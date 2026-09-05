f003f4bbf4a03fe23479bed7e29626e09d483f06
DUDI-CHARANISRI <113605400+DUDI-CHARANISRI@users.noreply.github.com>
2026-09-05 16:06:44 +0000

    intil project setup

---
+++ src/test/java/com/example/cron_scheduler/CronSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cron_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CronSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
